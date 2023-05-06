f49991a16caec329e3e5241688ea778ecbff7ae0
Poujhit μ <poujhit.dev@gmail.com>
2023-05-06 16:38:24 +0000

    Updated the url

--- packages/livechat/src/api.ts
+++ packages/livechat/src/api.ts
@@ -2,7 +2,9 @@ import LivechatClient from '@rocket.chat/sdk/lib/clients/Livechat';
 import { parse } from 'query-string';
 
 const host =
-	window.SERVER_URL || parse(window.location.search).serverUrl || (process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : null);
+	window.SERVER_URL ||
+	parse(window.location.search).serverUrl ||
+	(process.env.NODE_ENV === 'development' ? 'https://3000-poujhit-rocketchat-ofued1iih3b.ws-us96b.gitpod.io' : null);
 export const useSsl = Boolean((Array.isArray(host) ? host[0] : host)?.match(/^https:/));
 
 export const Livechat = new LivechatClient({ host, protocol: 'ddp', useSsl });
